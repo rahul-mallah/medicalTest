65eaf302a683258b7e594268972d1e49b6f28516
Jasperjwk <jasperjwko@gmail.com>
2020-12-20 11:35:55 +0800

    Component NavBar For My Profile PAGE (Back, Change Password, Logout)

---
+++ project/src/components/SidebarData.js
@@ -0,0 +1,26 @@
+import React from 'react'; 
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+    {
+        title: 'Back',
+        path: '/Back',
+        //icon: <AiIcons.AiFillHome />, //home icon
+        cName: 'nav-text'
+    },
+    {
+        title: 'Change Password',
+        path: '/changePW',
+        cName: 'nav-text'
+    },
+    {
+        title: 'Logout',
+        path: '/Logout',
+        cName: 'nav-text'
+    },
+]
+
+
+
