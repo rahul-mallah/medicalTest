0f5e2c4a65f8aa3a6611fd7fb52c065723b1eaca
Zknnn <zkn850@uowmail.edu.au>
2020-12-26 22:37:24 +0800

    resetpassword ok

--- project/src/User/ResetPasswordUI.js
+++ project/src/User/ResetPasswordUI.js
@@ -1,36 +1,55 @@
-import React from 'react'
-import {Form, Card, Container} from 'react-bootstrap'
-import {Link} from 'react-router-dom';
-function ResetPasswordUI() {
-    return (
-        <div style={{backgroundImage: `url("https://i.ibb.co/yRDqQHh/pexels-karolina-grabowska-4021769.jpg")`,
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundSize: 'cover'}}>
-            <Container className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh"}}>
-          <div className="w-100" style={{maxWidth: "400px"}}>
-            <Card>
-             <Card.Body>
-                 <h2 className= "text-center mb-4"> Reset Password</h2>
-                 <Form>
-                     <Form.Group id = "email">
-                        <Form.Label>Enter Your Email Address</Form.Label>
-                        <Form.Control type="email" required/>
-                     </Form.Group>
-                 </Form>
-                 <div className="w-100 text-center mt-2">
-                Forgot Password?
-            </div>
-            <div className="w-100 text-center mt-2">
-                Don't have an account? <Link to="/register"><u>Join Now!</u></Link>
-            </div>
-             </Card.Body>
-            </Card>
-            </div>
-            </Container>
-        </div>
-    )
-}
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from '../util/Auth';
+import { Link } from "react-router-dom"
 
-export default ResetPasswordUI
+export default function ResetPasswordUI() {
+    const emailRef = useRef("");
+    const { resetPassword } = useAuth("")
+    const [error, setError] = useState("")
+    const [message, setMessage] = useState("")
+    const [loading, setLoading] = useState(false)
+  
+    async function handleSubmit(e) {
+      e.preventDefault()
+  
+      try {
+        setMessage("")
+        setError("")
+        setLoading(true)
+        await resetPassword(emailRef.current.value)
+        setMessage("Successful! Check your inbox for further Instructions")
+      } catch {
+        setError("Request Fail. Please enter a registered email.")
+      }
+  
+      setLoading(false)
+    }
+  
+    return (
+      <>
+        <Card>
+          <Card.Body>
+            <h2 className="w-100 text-center mt-2">Reset Password</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            {message && <Alert variant="success">{message}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Enter Your Registered Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Button disabled={loading} className="w-100 text-center mt-2" type="submit">
+                Reset Password Now
+              </Button>
+            </Form>
+            <div className="w-100 text-center mt-2">
+              <Link to="/login">Back to Login Screen</Link>
+            </div>
+          </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">
+          Need an account? <Link to="/signup">Sign Up Now!</Link>
+        </div>
+      </>
+    )
+  }
