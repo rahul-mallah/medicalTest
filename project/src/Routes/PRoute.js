d780dee0b3be183d1fe4a21a432bbf364380b2e2
Hotpot94 <mallahrahul@gmail.com>
2021-01-21 13:49:30 +0800

    test

--- project/src/Routes/PRoute.js
+++ project/src/Routes/PRoute.js
@@ -82,11 +82,6 @@ const PRoute = () =>
             {/* // Individual Doctor Profile */}
             <PatientRoute exact path={`${path}/doctorProfile/:id`} component={DoctorProfile} role={user.Role}/>
 
-            // Display error if path does not match
-            <Route path="*">
-                <NoMatch />
-            </Route>
-
             </AuthProvider>
             {/* // Display error if path does not match */}
             <Route path="*">
