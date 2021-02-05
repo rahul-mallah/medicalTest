b6f744f6757409dc0031f2ab94259364a2539b33
lishanlim@outlook.com <lslim014@mymail.sim.edu.sg>
2021-02-05 15:38:07 +0800

    Med Adm secure route

--- project/src/util/SystemAdminRoute.js
+++ project/src/util/SystemAdminRoute.js
@@ -32,6 +32,15 @@ function SystemAdminRoute({ component: Component, role, ...rest }) {
        ></Route>
        )}
 
+        {role === "Medical Admin" && (
+       <Route
+       {...rest}
+       render={props => {
+           return (<Redirect to="/MedAdm" />)
+       }}
+       ></Route>
+       )}
+
         </div>
     )
 }
